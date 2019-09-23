bcb52099e34c22ea332f7c991693cf5ff29f5712
zhenglongfei <i@merryyou.cn>
2019-09-23 14:57:11 +0800

    testFile

--- src/main/java/cn/merryyou/file/ChangeFileName.java
+++ src/main/java/cn/merryyou/file/ChangeFileName.java
@@ -11,9 +11,10 @@ import java.util.Objects;
  */
 public class ChangeFileName {
     static String newString = "";//新字符串,如果是去掉前缀后缀就留空，否则写上需要替换的字符串
-    static String oldString = "【十次方禁广告闲聊讨论群863676367】";//要被替换的字符串
-    static String oldString_2 = "_十次方学习禁广告闲聊群863676367";
-    static String dir = "/Volumes/Elements/视频/十次方(前后端)无加密 完整版";//文件所在路径，所有文件的根目录，记得修改为你电脑上的文件所在路径
+    static String oldString = "[www.javazx.com]";//要被替换的字符串
+    static String rmFileName = "《javazx.com》Java自学网_用户必读.txt";
+    //    static String oldString_2 = "_十次方学习禁广告闲聊群863676367";
+    static String dir = "/Volumes/Elements/视频/javazx/20天Java开发大型电商系统谷粒商城实战课程";//文件所在路径，所有文件的根目录，记得修改为你电脑上的文件所在路径
 
     public static void main(String[] args) throws IOException {
         recursiveTraversalFolder(dir);//递归遍历此路径下所有文件夹
@@ -41,9 +42,9 @@ public class ChangeFileName {
                         newDir = reName(newDir, fileName, parentPath, file);
 
                         System.out.println("文件夹:" + file.getAbsolutePath() + "，继续递归！");
-                        if(Objects.isNull(newDir)){
+                        if (Objects.isNull(newDir)) {
                             recursiveTraversalFolder(file.getAbsolutePath());
-                        }else{
+                        } else {
                             recursiveTraversalFolder(newDir.getAbsolutePath());
                         }
                     } else {//是文件，判断是否需要重命名
@@ -61,15 +62,14 @@ public class ChangeFileName {
     private static File reName(File newDir, String fileName, File parentPath, File file) {
         String newName;
         if (fileName.contains(oldString)) {//文件名包含需要被替换的字符串
-            newName = fileName.replaceAll(oldString, newString);//新名字
+            newName = fileName.replace(oldString, newString);//新名字
             newDir = new File(parentPath + "/" + newName);//文件所在文件夹路径+新文件名
             file.renameTo(newDir);//重命名
             System.out.println("修改后：" + newDir);
-        }else if(fileName.contains(oldString_2)){
-            newName = fileName.replaceAll(oldString_2, newString);//新名字
-            newDir = new File(parentPath + "/" + newName);//文件所在文件夹路径+新文件名
-            file.renameTo(newDir);//重命名
-            System.out.println("修改后：" + newDir);
+//            file.delete();
+        } else if (fileName.equalsIgnoreCase(rmFileName)) {
+            newDir = new File(parentPath + "/" + fileName);
+            newDir.delete();
         }
         return newDir;
     }
